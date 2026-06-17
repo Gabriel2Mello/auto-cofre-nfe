0913a38254275ba7f7b57e1ce3701aeef5634c49
Gabriel Mello <gabriel.mellosjb@gmail.com>
2026-06-17 16:57:58 -0300

    Melhorado estrura e indentação do código

--- src/config.py
+++ src/config.py
@@ -6,9 +6,9 @@ CAMINHO_DOCUMENTO_ENTRADA = environ['CAMINHO_DOCUMENTO_ENTRADA']
 
 
 USER_AGENT = (
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-    'AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Chrome/145.0.0.0 Safari/537.36'
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+  'AppleWebKit/537.36 (KHTML, like Gecko) '
+  'Chrome/145.0.0.0 Safari/537.36'
 )
 
 URL_BASE = 'https://painel.cofrenfe.com.br'
@@ -16,20 +16,29 @@ CONTENT_TYPE = 'application/x-www-form-urlencoded; charset=UTF-8'
 ACCEPT = 'application/json, text/javascript, */*; q=0.01'
 REQUESTED_WITH = 'XMLHttpRequest'
 
+CNPJ = {
+  'MATRIZ': '09.034.052/0001-53',
+  'FILIAL': '09.034.052/0002-34'
+}
 
 COLUNAS = {
-    'nfe': 'recebimento_quando,emitente_nome,nfe_data,nro_nota,vlr_total,tipo,',
-    'cte': 'recebimento_quando,emitente_nome,destinatario_nome,nfe_data,nro_nota,vlr_total,tipo,tipo'
+  'nfe': 'recebimento_quando,emitente_nome,nfe_data,nro_nota,vlr_total,tipo,',
+  'cte': 'recebimento_quando,emitente_nome,destinatario_nome,nfe_data,nro_nota,vlr_total,tipo,tipo'
 }
 
 MONTHS = [
-    None,
-    'JANEIRO', 'FEVEREIRO', 'MARÇO', 'ABRIL',
-    'MAIO', 'JUNHO', 'JULHO', 'AGOSTO', 'SETEMBRO',
-    'OUTUBRO', 'NOVEMBRO', 'DEZEMBRO'
+  None,
+  'JANEIRO',
+  'FEVEREIRO',
+  'MARÇO',
+  'ABRIL',
+  'MAIO',
+  'JUNHO',
+  'JULHO',
+  'AGOSTO',
+  'SETEMBRO',
+  'OUTUBRO',
+  'NOVEMBRO',
+  'DEZEMBRO',
 ]
 
-CNPJ = {
-    'MATRIZ': '09.034.052/0001-53',
-    'FILIAL': '09.034.052/0002-34'
-}
--- src/interface.py
+++ src/interface.py
@@ -1,85 +1,87 @@
 from datetime import datetime
 
-from prompt_toolkit.shortcuts import radiolist_dialog
 from prompt_toolkit import prompt
+from prompt_toolkit.shortcuts import radiolist_dialog
 
-from src.config import (MONTHS)
+from src.config import MONTHS
 from src.utils import encerrar_programa
 
 
 def selecionar_mes():
-    meses = [(i, MONTHS[i]) for i in range(1,13)]
+  meses = [(i, MONTHS[i]) for i in range(1, 13)]
 
-    mes = radiolist_dialog(
-        title='Escolha o mês:',
-        values=meses
-    ).run()
+  mes = radiolist_dialog(
+    title='Escolha o mês:',
+    values=meses
+  ).run()
 
-    encerrar_programa(mes)
-    return mes
+  encerrar_programa(mes)
+  return mes
 
 
 def escolher_mes(titulo, texto):
-    mes_atual = datetime.today().month
+  mes_atual = datetime.today().month
 
-    mes = radiolist_dialog(
-        title=titulo,
-        text=texto,
-        values=[
-            (mes_atual, f'ATUAL ({MONTHS[mes_atual]})'),
-            ('outro', 'OUTRO')
-        ]
-    ).run()
+  mes = radiolist_dialog(
+    title=titulo,
+    text=texto,
+    values=[
+      (mes_atual, f'ATUAL ({MONTHS[mes_atual]})'),
+      ('outro', 'OUTRO')
+    ]
+  ).run()
 
-    encerrar_programa(mes)
+  encerrar_programa(mes)
 
-    if mes == 'outro':
-        mes = selecionar_mes()
+  if mes == 'outro':
+    mes = selecionar_mes()
 
-    return mes
+  return mes
 
 
 def input_dados():
-    tipo_input = prompt('Tipo 1(NFe) 2(CTe): ').strip()
-    tipo = 'nfe' if tipo_input == '1' else 'cte'
+  #tipo_input = prompt('Tipo 1(NFe) 2(CTe): ').strip()
+  #tipo = 'cte' if tipo_input == '2' else 'nfe'
+  tipo = 'nfe'
 
-    notas_input = prompt('Nota: ').strip()
-    notas = [n.strip() for n in notas_input.split(',') if n.strip()]
-    encerrar_programa(notas)
+  notas_input = prompt('Nota: ').strip()
+  notas = [n.strip() for n in notas_input.split(',') if n.strip()]
+  encerrar_programa(notas)
 
-    modo_input = prompt('Modo 1(Normal) 2(Manual): ').strip()
-    modo = 'NORMAL' if modo_input == '1' else 'MANUAL'
+  #modo_input = prompt('Modo 1(Normal) 2(Manual): ').strip()
+  #modo = 'MANUAL' if modo_input == '2' else 'NORMAL'
+  modo = 'NORMAL'
 
-    empresa = None
-    mes_nota = None
-    mes_pasta = None
+  empresa = None
+  mes_nota = None
+  mes_pasta = None
 
-    if modo == 'NORMAL':
-        empresa_input = prompt('Empresa 1(Matriz) 2(Filial): ').strip()
-        empresa = 'MATRIZ' if empresa_input == '1' else 'FILIAL'
+  if modo == 'NORMAL':
+    empresa_input = prompt('Empresa 1(Matriz) 2(Filial): ').strip()
+    empresa = 'MATRIZ' if empresa_input == '1' else 'FILIAL'
 
-        mes_atual = datetime.today().month
-        mes_nota = mes_atual
-        mes_pasta = mes_atual
-    else:
-        empresa = radiolist_dialog(
-            title='Empresa',
-            text='Empresa de faturamento:',
-            values=[
-                ('MATRIZ', 'MATRIZ'),
-                ('FILIAL', 'FILIAL')
-            ]
-        ).run()
-        encerrar_programa(empresa)
+    mes_atual = datetime.today().month
+    mes_nota = mes_atual
+    mes_pasta = mes_atual
+  else:
+    empresa = radiolist_dialog(
+      title='Empresa',
+      text='Empresa de faturamento:',
+      values=[
+        ('MATRIZ', 'MATRIZ'),
+        ('FILIAL', 'FILIAL')
+      ]
+    ).run()
+    encerrar_programa(empresa)
 
-        mes_nota = escolher_mes(
-            'Mês da Nota',
-            'Selecione o mês de faturamento da nota:'
-        )
-        mes_pasta = escolher_mes(
-            'Pasta Destino',
-            'Pasta destino:'
-        )
+    mes_nota = escolher_mes(
+      'Mês da Nota',
+      'Selecione o mês de faturamento da nota:'
+    )
+    mes_pasta = escolher_mes(
+      'Pasta Destino',
+      'Pasta destino:'
+    )
 
-    return notas, empresa, mes_nota, mes_pasta, tipo
+  return notas, empresa, mes_nota, mes_pasta, tipo
 

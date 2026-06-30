0bd0414e7876951e0de89019fb666cf5b8a3675a
Gabriel Mello <gabriel.mellosjb@gmail.com>
2026-06-30 10:25:20 -0300

    Criado class Config para os campos

--- src/config.py
+++ src/config.py
@@ -1,4 +1,5 @@
 from os import environ
+from typing import ClassVar, Dict, List
 
 
 def load_env(variable: str, default: str = "") -> str:
@@ -9,40 +10,39 @@ def load_env(variable: str, default: str = "") -> str:
 
 
 class Config:
-  SENHA_COFRE: str
-  CAMINHO_DOCUMENTO_ENTRADA: str
+  SENHA_COFRE: str = ""
+  CAMINHO_DOCUMENTO_ENTRADA: str = ""
 
+  URL_BASE: ClassVar[str] = 'https://painel.cofrenfe.com.br'
+  CONTENT_TYPE: ClassVar[str] = 'application/x-www-form-urlencoded; charset=UTF-8'
+  ACCEPT: ClassVar[str] = 'application/json, text/javascript, */*; q=0.01'
+  REQUESTED_WITH: ClassVar[str] = 'XMLHttpRequest'
 
-URL_BASE = 'https://painel.cofrenfe.com.br'
-CONTENT_TYPE = 'application/x-www-form-urlencoded; charset=UTF-8'
-ACCEPT = 'application/json, text/javascript, */*; q=0.01'
-REQUESTED_WITH = 'XMLHttpRequest'
+  CNPJ: ClassVar[Dict[str, str]] = {
+    'MATRIZ': '09.034.052/0001-53',
+    'FILIAL': '09.034.052/0002-34'
+  }
 
-CNPJ = {
-  'MATRIZ': '09.034.052/0001-53',
-  'FILIAL': '09.034.052/0002-34'
-}
+  COLUNAS: ClassVar[Dict[str, str]] = {
+    'nfe': 'recebimento_quando,emitente_nome,nfe_data,nro_nota,vlr_total,tipo,',
+    'cte': 'recebimento_quando,emitente_nome,destinatario_nome,nfe_data,nro_nota,vlr_total,tipo,tipo'
+  }
 
-COLUNAS = {
-  'nfe': 'recebimento_quando,emitente_nome,nfe_data,nro_nota,vlr_total,tipo,',
-  'cte': 'recebimento_quando,emitente_nome,destinatario_nome,nfe_data,nro_nota,vlr_total,tipo,tipo'
-}
-
-MONTHS = [
-  None,
-  'JANEIRO',
-  'FEVEREIRO',
-  'MARÇO',
-  'ABRIL',
-  'MAIO',
-  'JUNHO',
-  'JULHO',
-  'AGOSTO',
-  'SETEMBRO',
-  'OUTUBRO',
-  'NOVEMBRO',
-  'DEZEMBRO',
-]
+  MONTHS: ClassVar[List[str]] = [
+    '',
+    'JANEIRO',
+    'FEVEREIRO',
+    'MARÇO',
+    'ABRIL',
+    'MAIO',
+    'JUNHO',
+    'JULHO',
+    'AGOSTO',
+    'SETEMBRO',
+    'OUTUBRO',
+    'NOVEMBRO',
+    'DEZEMBRO',
+  ]
 
 def init_config() -> None:
   Config.SENHA_COFRE = load_env('SENHA_COFRE')

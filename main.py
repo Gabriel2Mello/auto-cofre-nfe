788a2769208cea33bd7c73306cb468fcdf387cd0
Gabriel Mello <gabriel.mellosjb@gmail.com>
2026-07-08 10:28:56 -0300

    Quebrado função processar_nota em menores readequação a remoção de interface

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ from src.core import (
   trocar_empresa,
   processar_nota,
   marcar_flag,
+  baixar_arquivos,
 )
 
 
@@ -36,50 +37,58 @@ def main() -> None:
   emitente_handler = EmitenteHandler()
   start_time = perf_counter()
 
-  try:
-    with TimeoutScraper() as session:
-      html_login = login(session, config.senha_cofre)
-      empresas_href = extrair_empresas_href(html_login)
-      trocar_empresa(session, empresa, empresas_href)
+  with TimeoutScraper() as session:
+    html_login = login(session, config.senha_cofre)
+    empresas_href = extrair_empresas_href(html_login)
+    trocar_empresa(session, empresa, empresas_href)
 
-      print('Sincronizado...')
-      sleep(0.2)
-      ver_arquivos(session, tipo)
+    print('Sincronizado...')
+    sleep(0.2)
+    ver_arquivos(session, tipo)
 
-      for nota in notas:
-        print(f'\nProcessando: {nota}')
-        try:
-          # TODO: Quebrar essa função em menores
-          processar_nota(
-            session,
-            nota,
-            empresa,
-            tipo,
-            mes_nota,
-            mes_pasta,
-            ano_nota,
-            ano_pasta,
-            emitente_handler,
-            config.caminho_documento_entrada,
-          )
-          baixar_arquivos()
-          nome_emitente = emitente_handler.get_nome(dados['emitente'])
-          salvar_arquivos()
-          marcar_flag()
-        except Timeout as e:
-          handle_error(e, 'Site demorou a responder')
-        except HTTPError as e:
-          handle_error(e, 'Erro HTTP')
-        except RequestException as e:
-          handle_error(e, 'Erro no site')
-        except (KeyError, ValueError) as e:
-          handle_error(e, 'Valor faltando/inadequado')
-        except Exception as e:
-          handle_error(e, f'Erro na nota {nota}')
+    for nota in notas:
+      print(f'\nProcessando: {nota}')
+      try:
+        dados = processar_nota(
+          session,
+          nota,
+          tipo,
+          mes_nota,
+          ano_nota,
+        )
+        xml, pdf = baixar_arquivos(
+          session,
+          dados['empresa_id'],
+          dados['chave'],
+          tipo,
+        )
+        emitente = emitente_handler.get_nome(dados['emitente'])
+        salvar_arquivos(
+          xml,
+          pdf,
+          emitente,
+          nota,
+          empresa,
+          mes_pasta,
+          str(ano_pasta),
+          tipo,
+          config.caminho_documento_entrada,
+        )
+        marcar_flag(session, dados['codigo_arquivo'])
+        sleep(0.2)
 
-  finally:
+      except Timeout as e:
+        handle_error(e, 'Site demorou a responder')
+      except HTTPError as e:
+        handle_error(e, 'Erro HTTP')
+      except RequestException as e:
+        handle_error(e, 'Erro no site')
+      except (KeyError, ValueError) as e:
+        handle_error(e, 'Valor faltando/inadequado')
+      except Exception as e:
+        handle_error(e, f'Erro na nota {nota}')
+
     emitente_handler.close()
-
   elapsed_time = perf_counter() - start_time
   print(f'\nTerminado em {elapsed_time:0.2f} segundos')
   pause()
